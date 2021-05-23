21dd040fdaffdc3a5d9b483d9bd8f8172c2133da
Alistair Smith <alistairsmith01@gmail.com>
2021-05-24 00:19:59 +0100

    fix: Page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
+import React from "react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/main.css";
 import "tailwindcss/tailwind.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Kaito</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
